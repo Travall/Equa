9e8ba4476de59f41d675737988cdabe37c5afc6b
AndEditor7 <35620587+AndEditor7@users.noreply.github.com>
2020-10-20 19:41:54 +1100

    Update texture

--- core/src/com/travall/game/utils/Utils.java
+++ core/src/com/travall/game/utils/Utils.java
@@ -6,11 +6,11 @@ import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 
 public class Utils {
 
-    public static double normalize(double Input, int max) {
-        return ((Input - -1) / (1 - -1) * (max - 0) + 0);
-    }
-    
-    public static int[] locateAttributes(ShaderProgram shader, VertexAttributes attributes) {
+	public static double normalize(double Input, int max) {
+		return ((Input - -1) / (1 - -1) * (max - 0) + 0);
+	}
+
+	public static int[] locateAttributes(ShaderProgram shader, VertexAttributes attributes) {
 		final int s = attributes.size();
 		final int[] locations = new int[s];
 		for (int i = 0; i < s; i++) {
@@ -19,15 +19,17 @@ public class Utils {
 		}
 		return locations;
 	}
-    
-    public static float gamma(double num) {
-    	return (float)Math.pow(num, 2.2);
-    }
-    
-    public static int createANDbits(final int bitSize) {
-    	return -1 >>> 32 - bitSize;
-    }
 
-	public static boolean inBounds(int index, int length) {return (index >= 0) && (index < length);}
+	public static float gamma(double num) {
+		return (float)Math.pow(num, 2.2);
+	}
+
+	public static int createANDbits(final int bitSize) {
+		return -1 >>> 32 - bitSize;
+	}
+
+	public static boolean inBounds(int index, int length) {
+		return (index >= 0) && (index < length);
+	}
 
 }
